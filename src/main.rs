9e9f75d3e64adb044decc5b1cca06bb5830f4973
Artem Lukin <77511570+l0wgear@users.noreply.github.com>
2025-06-29 16:15:33 +0200

    Add early decoder stopping Add detokenization of predicted indexes to string

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,97 @@
 mod hf;
 mod model;
+use std::path::PathBuf;
+
 use hf_hub::api::sync::{Api, ApiError};
 use image::imageops::FilterType;
-use ort::{environment::Environment, session::Session};
+use ort::{
+    environment::Environment,
+    session::{Session, builder::GraphOptimizationLevel},
+};
+use tokenizers::{Tokenizer, TokenizerBuilder};
 
 use crate::model::{Decoder, Encoder};
+
+struct ModelDir {
+    encoder_path: PathBuf,
+    decoder_path: PathBuf,
+    tokenizer_config_path: PathBuf,
+}
+
+impl ModelDir {
+    fn new(paths: &Vec<PathBuf>) -> anyhow::Result<Self> {
+        let mut encoder_path = None;
+        let mut decoder_path = None;
+        let mut tokenizer_config_path = None;
+
+        for p in paths.iter() {
+            if p.ends_with("encoder_model.onnx") {
+                encoder_path = Some(p.clone());
+            }
+            if p.ends_with("decoder_model.onnx") {
+                decoder_path = Some(p.clone());
+            }
+            if p.ends_with("tokenizer.json") {
+                tokenizer_config_path = Some(p.clone());
+            }
+        }
+
+        if encoder_path.is_none() || decoder_path.is_none() || tokenizer_config_path.is_none() {
+            return Err(anyhow::anyhow!(
+                "Missing encoder, decoder, or tokenizer model"
+            ));
+        }
+
+        Ok(Self {
+            encoder_path: encoder_path.unwrap(),
+            decoder_path: decoder_path.unwrap(),
+            tokenizer_config_path: tokenizer_config_path.unwrap(),
+        })
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     ort::init().with_name("test").commit()?;
 
     let paths = hf::pull_model("l0wgear/manga-ocr-2025-onnx")?;
-    let mut encoder_path = None;
-    let mut decoder_path = None;
+    let model_files = ModelDir::new(&paths)?;
 
-    for p in paths.iter() {
-        if p.ends_with("encoder_model.onnx") {
-            encoder_path = Some(p.clone());
-        }
-        if p.ends_with("decoder_model.onnx") {
-            decoder_path = Some(p.clone());
-        }
+    let tokenizer = Tokenizer::from_file(model_files.tokenizer_config_path);
+    if tokenizer.is_err() {
+        return Err(anyhow::anyhow!("Failed to load tokenizer"));
     }
 
-    if encoder_path.is_none() || decoder_path.is_none() {
-        return Err(anyhow::anyhow!("Missing encoder or decoder model"));
-    }
+    let mut tokenizer = tokenizer.unwrap();
 
-    let s = Session::builder()?.commit_from_file(encoder_path.unwrap())?;
+    let end_tokens = tokenizer.encode("[SEP]", false).unwrap();
+    let end_token_ids = end_tokens.get_ids();
+    println!("{:?}", end_token_ids);
+
+    let s = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .commit_from_file(model_files.encoder_path)?;
     let mut encoder = Encoder::new(s);
-    let s = Session::builder()?.commit_from_file(decoder_path.unwrap())?;
-    let mut decoder = Decoder::new(s);
+    let s = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .commit_from_file(model_files.decoder_path)?;
+    let decoder = Decoder::new(s, end_token_ids);
 
     let img = image::ImageReader::open("./test1.png")?.decode()?;
 
     let out = encoder.encode(img)?;
-    println!("{:?}", out);
-    let dec_out = decoder.decode(out);
-    println!("{:?}", dec_out);
+    // println!("{:?}", out);
+    let dec_out = decoder.decode(out, 300)?;
+    // println!("{:?}", dec_out);
+
+    let idx: Vec<u32> = dec_out.iter().map(|i| *i as u32).collect();
+    let decoded = tokenizer.decode(&idx, true);
+
+    if decoded.is_err() {
+        return Err(anyhow::anyhow!("Failed to decode"));
+    }
+    let decoded = decoded.unwrap().replace(" ", "");
+
+    println!("{:?}", decoded);
+
     Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::{path::PathBuf, sync::Arc};
 
 use image::{EncodableLayout, RgbImage, imageops::FilterType};
@@ -43,8 +44,6 @@ impl Encoder {
         let resized = input.resize_exact(224, 224, FilterType::Nearest);
         let resized = resized.to_rgb8();
 
-        resized.save("./test1_out.png");
-
         let scale = 0.00392156862745098;
 
         let arr = (rgb_to_array(&resized)? * scale - 0.5) / 0.5;
@@ -54,11 +53,6 @@ impl Encoder {
             .run(ort::inputs![TensorRef::from_array_view(&arr)?])?;
         Ok(outputs[0].try_extract_array::<f32>()?.to_owned())
     }
-}
-
-pub struct Decoder {
-    session: Session,
-    max_end_token_repeats: usize,
 }
 
 #[derive(Debug)]
@@ -83,34 +77,52 @@ fn last_token_idx(input: ArrayD<f32>) -> Result<i64, DecodeError> {
     Ok(idx as i64)
 }
 
+pub struct Decoder {
+    session: RefCell<Session>,
+    max_end_token_repeats: usize,
+    end_token_ids: Vec<i64>,
+}
+
 impl Decoder {
-    pub fn new(session: Session) -> Self {
+    pub fn new(session: Session, end_token_ids: &[u32]) -> Self {
         Self {
-            session,
+            session: RefCell::new(session),
             max_end_token_repeats: 3,
+            end_token_ids: end_token_ids.iter().map(|i| *i as i64).collect(),
         }
     }
 
-    pub fn decode(&mut self, input: ArrayD<f32>) -> anyhow::Result<Vec<i64>> {
+    fn stop_decoding(&self, tokens: &Vec<i64>) -> bool {
+        if tokens.len() < self.max_end_token_repeats {
+            return false;
+        }
+        let mut count = 0;
+        for i in tokens.len() - self.max_end_token_repeats..tokens.len() {
+            if self.end_token_ids.contains(&(tokens[i])) {
+                count += 1;
+            }
+        }
+        count >= self.max_end_token_repeats
+    }
+
+    pub fn decode(&self, input: ArrayD<f32>, max_tokens: usize) -> anyhow::Result<Vec<i64>> {
         // let input = ndarray::Array4::<f32>::zeros((1, 64, 64, 3));
         // let mut output_vec = vec![];
         let mut input_ids = vec![2i64];
-        for _ in 0..100 {
+        let mut session = self.session.borrow_mut();
+        for _ in 0..max_tokens {
             let input_ref = TensorRef::from_array_view(&input)?;
             let input_ids_tensor = Tensor::from_array(([1, input_ids.len()], input_ids.clone()))?;
-            let outputs = self.session.run(ort::inputs! {
+            let outputs = session.run(ort::inputs! {
                 "encoder_hidden_states" => input_ref,
                 "input_ids" => input_ids_tensor,
             })?;
             let arr = outputs[0].try_extract_array::<f32>()?.to_owned();
-
-            // let shape = arr.shape();
-            // println!("Shape: {:?}", shape);
-            // println!("Item: {:?}", arr[[0, 0, 0]]);
             let idx = last_token_idx(arr)?;
             input_ids.push(idx);
-            // let idx = arr[shape[0] - 1].argmax()
-            // output_vec.push(arr);
+            if self.stop_decoding(&input_ids) {
+                break;
+            }
         }
         Ok(input_ids.clone())
     }
