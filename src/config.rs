a742390aee232705e92ddd3c6c7ac0def6075371
Artem Lukin <77511570+l0wgear@users.noreply.github.com>
2025-06-30 19:58:00 +0200

    Add tests

--- src/config.rs
+++ src/config.rs
@@ -23,3 +23,32 @@ impl GenerationConfig {
         Ok(config)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_correct_config() {
+        let path = PathBuf::from("tests/fixtures/generation_config.json");
+        let res = GenerationConfig::from_file(&path);
+        assert!(res.is_ok());
+        let config = res.unwrap();
+        assert_eq!(config.decoder_start_token_id, 2);
+        assert_eq!(config.early_stopping, true);
+        assert_eq!(config.eos_token_id, 3);
+        assert_eq!(config.length_penalty, 2.0);
+        assert_eq!(config.max_length, 300);
+        assert_eq!(config.no_repeat_ngram_size, 3);
+        assert_eq!(config.num_beams, 4);
+        assert_eq!(config.pad_token_id, 0);
+        assert_eq!(config.transformers_version, "4.52.4");
+    }
+
+    #[test]
+    fn test_load_incorrect_config() {
+        let path = PathBuf::from("tests/fixtures/generation_config_incorrect.json");
+        let res = GenerationConfig::from_file(&path);
+        assert!(res.is_err());
+    }
+}
--- src/hf.rs
+++ src/hf.rs
@@ -11,3 +11,23 @@ pub fn pull_model(name: &str) -> Result<Vec<PathBuf>, ApiError> {
     }
     Ok(local_paths)
 }
+
+#[cfg(all(test, feature = "huggingface-tests"))]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pull_model_nonexistent() {
+        let model_name = "~~nonexistent";
+        let res = pull_model(model_name);
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_pull_model() {
+        let model_name = "hf-tiny-model-private/tiny-random-YosoModel";
+        let res = pull_model(model_name);
+        assert!(res.is_ok());
+        assert!(!res.unwrap().is_empty());
+    }
+}
