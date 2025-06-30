cd10c4a095db4c638bba2307a752b0b4f5d84c0d
Artem Lukin <77511570+l0wgear@users.noreply.github.com>
2025-06-30 20:02:54 +0200

    Add cli interface

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,25 @@ mod hf;
 mod model;
 
 use crate::model::OCRModel;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "l0wgear/manga-ocr-2025-onnx")]
+    model: String,
+
+    #[arg(short, long)]
+    image: PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
-    let model = OCRModel::from_name_or_path("l0wgear/manga-ocr-2025-onnx")?;
+    let args = Args::parse();
 
-    let img = image::ImageReader::open("./test1.png")?.decode()?;
+    let model = OCRModel::from_name_or_path(&args.model)?;
+
+    let img = image::ImageReader::open(&args.image)?.decode()?;
     let text = model.run(&img);
 
     match text {
