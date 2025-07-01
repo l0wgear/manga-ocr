ead53755e94126d6cba06918990953fe43555256
Artem Lukin <77511570+l0wgear@users.noreply.github.com>
2025-07-01 19:06:29 +0200

    Move clipboard handling to dedicated struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+mod clipboard;
 mod config;
 mod hf;
 mod model;
 
+use crate::clipboard::ClipboardHandler;
 use crate::model::OCRModel;
-use arboard::{Clipboard, ImageData};
 use clap::{Parser, ValueEnum};
-use image::{DynamicImage, ImageBuffer, Rgba};
 use std::fmt::Display;
-use std::hash::{Hash, Hasher};
-use std::{hash::DefaultHasher, path::PathBuf, thread::sleep, time::Duration};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -21,6 +20,9 @@ struct Args {
 
     #[arg(long, default_value_t = Mode::Clipboard)]
     mode: Mode,
+
+    #[arg(long, default_value = "1.0")]
+    refresh_timeout: f64,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -37,26 +39,10 @@ impl Display for Mode {
     }
 }
 
-fn to_dyn_image(arboard_image: ImageData) -> Option<DynamicImage> {
-    let image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(
-        arboard_image.width as u32,
-        arboard_image.height as u32,
-        arboard_image.bytes.into_owned(),
-    )?;
-
-    // Convert the ImageBuffer to a DynamicImage
-    Some(DynamicImage::ImageRgba8(image_buffer))
-}
-
-fn hash(data: &[u8]) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    data.hash(&mut hasher);
-    hasher.finish()
-}
-
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
+    println!("Initializing model...");
     let model = OCRModel::from_name_or_path(&args.model)?;
 
     match args.mode {
@@ -74,34 +60,15 @@ fn main() -> anyhow::Result<()> {
             }
         }
         Mode::Clipboard => {
-            let mut clipboard = Clipboard::new()?;
-            let mut old_hash = match clipboard.get_image() {
-                Ok(img) => Some(hash(&img.bytes)),
-                Err(_) => None,
-            };
+            let mut clipboard = ClipboardHandler::new(args.refresh_timeout)?;
+            println!("Ready to do OCR");
+
             loop {
-                sleep(Duration::from_secs_f32(1.0));
-
-                let img = clipboard.get_image();
-                if img.is_err() {
-                    // println!("Error getting image from clipboard");
-                    continue;
-                }
-                let img = img.unwrap();
-
-                let new_hash = hash(&img.bytes);
-                if old_hash.is_some() && new_hash == old_hash.unwrap() {
-                    continue;
-                }
-                old_hash = Some(new_hash);
-                let dyn_img = to_dyn_image(img);
-                if let Some(dyn_img) = dyn_img {
-                    let text = model.run(&dyn_img);
+                let image = clipboard.get_image();
+                if let Some(img) = image {
+                    let text = model.run(&img);
                     match text {
-                        Ok(text) => {
-                            println!("{}", text);
-                            let _ = clipboard.set_text(text);
-                        }
+                        Ok(text) => println!("{}", text),
                         Err(err) => println!("Error: {:?}", err),
                     }
                 }
